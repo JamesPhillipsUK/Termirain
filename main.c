aae69be221a6069e435ef74eab3d3c7dad394b13
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2020-05-16 21:13:34 +0100

    Added copyright notice.

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 /**
+ * Termirain.
+ * An animated screensaver for *Nix TUIs.
+ * 
+ * Copyright (C) 2020 Jesse Phillips, Released under the GNU GPL v3.0 or later.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.  This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  
+ * You should have received a copy of the GNU General Public License along with this program.  
+ * If not, see <http://www.gnu.org/licenses/>.
+**/
+/**
  * main.c - the main controller for termirain.
- * To compile on *Nix, using GCC - "gcc -std=gnu11 -o /usr/bin/rain main.c -lncurses"
+ * @author Jesse Phillips
  **/
 #include <stdio.h>/* I/O Library */
 #include <stdlib.h>/* Standard Library */
